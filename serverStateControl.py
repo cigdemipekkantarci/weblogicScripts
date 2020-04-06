bbfd3d6de68f20e720c5f2f59db3aafc8c3a085c
Ipek Kantarci <ipekantarci@gmail.com>
2020-04-06 18:51:42 +0300

    Include server startup

--- serverStateControl.py
+++ serverStateControl.py
@@ -1,41 +1,63 @@
-# This script shows the state of weblogic servers in a domain. Don't forget to write your environment credentials in connect() function.
-# How to run?   $WL_HOME/oracle_common/common/wlst.sh /home/oracle/serverStateControl.py
+"""
+This scipt is used to show the status and start time of WebLogic servers. If a server is not running and is not in the 'blacklist', startup command is given.
 
-def serverStatus():
+NOTES:
+- ADmin server connection information and 'blacklist' (if any exist) should be adjusted according to environment
+"""
+
+def serverStatus(blacklist):
     #Get list of servers in current domain
     domainRuntime()
     servers = cmo.getServerLifeCycleRuntimes()
-    print "----------------------------------------------------------"
+    print "--------------------------------------------------------------------"
     print "\t" + "Server status in " + cmo.getName() + " domain "
-    print "----------------------------------------------------------"
+    print "--------------------------------------------------------------------"
     for server in servers:
-    	#Get Name and State of each server
+        #Get Name and State of each server
         serverState = server.getState()
+        serverName = server.getName()
         if serverState == "RUNNING":
-        	print server.getName() + '\t:\033[1;32m' + serverState + '\033[0m'
+                startTime = getActivationTime(serverName)
+                print serverName + '\t:\033[1;32m' + serverState +  '\033[0m' + '\tStart Time\t:' + startTime
         elif serverState == "STARTING":
-       		print server.getName() + '\t:\033[1;33m' + serverState + '\033[0m'
+                print serverName + '\t:\033[1;33m' + serverState + '\033[0m'
         elif serverState == "UNKNOWN":
-        	print server.getName() + '\t:\033[1;34m' + serverState + '\033[0m'
+                print serverName + '\t:\033[1;34m' + serverState + '\033[0m'
+        elif serverState == "SHUTDOWN" or "FAILED_NOT_RESTARTABLE":
+                print serverName + '\t:\033[1;31m' + serverState + '\033[0m'
+                startManagedSrv(serverName,blacklist)
+                serverState = server.getState()
+                if serverState == "RUNNING":
+                    startTime = getActivationTime(serverName)
+                    print serverName + '\t:\033[1;32m' + serverState +  '\033[0m' + '\tStart Time\t:' + startTime 
         else:
-        	print server.getName() + '\t:\033[1;31m' + serverState + '\033[0m'
-    print "----------------------------------------------------------"
-    quit()
+                print serverName + '\t:\033[1;31m' + serverState + '\033[0m'
+    print "--------------------------------------------------------------------"
 
-def quit():
-    print '\033[1;35mTo re-run the script HIT any key..\033[0m'
-    Ans = raw_input("Are you sure quit from WLST? (y/n)")
-    if (Ans == 'y'):
-        disconnect()
-        stopRedirect()
-        exit()
-    else:
-        serverStatus()
+def startManagedSrv(serverName,blacklist):
+    try:
+        if serverName not in blacklist:
+            print serverName + ' is not in blacklist. Starting up...'
+            start(serverName,'Server')
+        else:
+            print serverName + ' is in blacklist. No start operation will be performed.'
+    except Exception,e:
+        print 'Exception while starting managed server!'       
+        dumpStack()
 
+def getActivationTime(serverName):
+    import time
+    cd('ServerRuntimes/'+serverName)
+    tmp = cmo.getActivationTime()
+    epochTime = str(tmp)[:10]
+    activationTime = time.ctime(int(epochTime))    
+    return activationTime          
+        
 try:
     connect("weblogic","weblogic123","t3://localhost:7001")
 except Exception,e:
-    print "Unable to connect Admin Server!"
+    print "Unable to connect to Admin Server!"
     exit()
 
-serverStatus()
+blacklist = ['server_1','AdminServer']
+serverStatus(blacklist)
